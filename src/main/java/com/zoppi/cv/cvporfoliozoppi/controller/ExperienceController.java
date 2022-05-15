cfe3ee4c52b66e9c56e5f7bba23a4dbbf2938390
Grecia Zoppi <zoppi.grecia@gmail.com>
2022-05-15 21:39:58 +0200

    actualizar angular url

--- src/main/java/com/zoppi/cv/cvporfoliozoppi/controller/ExperienceController.java
+++ src/main/java/com/zoppi/cv/cvporfoliozoppi/controller/ExperienceController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://zoppi-cv.web.app")
 @RequestMapping("/experience")
 public class ExperienceController {
 
