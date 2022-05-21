8d49fc02e8911778a85a94f48ac101c16c0641ac
Grecia Zoppi <zoppi.grecia@gmail.com>
2022-05-22 00:59:54 +0200

    agregar datos iniciales para H2

---
+++ src/main/resources/data.sql
@@ -0,0 +1,38 @@
+INSERT INTO Person (profession, name, surname, description)
+VALUES ('Programador Full Stack Web Jr.', 'Grecia Estrella', 'Zoppi', 'Profesional egresada de la UNC en Ciencias Económicas. Egresada con conocimientos de JAVA Script y Ruby en el Programa #SE PROGRAMAR. Cursante del programa #YO PROGRAMO.');
+
+INSERT INTO Experience (rol, start_date, end_date, person_id)
+VALUES ('Analista de Impuestos', '2012-05-01', '2020-03-03', 1);
+
+INSERT INTO Education (title, institute, start_date, end_date, person_id)
+VALUES ('Contador Publico', 'UNC', '2008-03-01', '2014-09-18', 1);
+
+INSERT INTO Skill (category, type, level, person_id)
+VALUES ('Idioma', 'Ingles', 'Intermedio', 1);
+
+INSERT INTO Skill (category, type, level, person_id)
+VALUES ('Idioma', 'Español', 'Avanzado', 1);
+
+INSERT INTO Skill (category, type, level, person_id)
+VALUES ('Lenguaje de Programacion', 'Java', 'Basico', 1);
+
+INSERT INTO Skill (category, type, level, person_id)
+VALUES ('Lenguaje de Programacion', 'Ruby', 'Basico', 1);
+
+INSERT INTO Skill (category, type, level, person_id)
+VALUES ('Lenguaje de Programacion', 'CSS', 'Basico', 1);
+
+INSERT INTO Skill (category, type, level, person_id)
+VALUES ('Lenguaje de Programacion', 'HTML', 'Basico', 1);
+
+INSERT INTO Skill (category, type, level, person_id)
+VALUES ('Lenguaje de Programacion', 'HTML', 'Basico', 1);
+
+INSERT INTO Skill (category, type, level, person_id)
+VALUES ('Lenguaje de Programacion', 'Java Script', 'Basico', 1);
+
+INSERT INTO Skill (category, type, level, person_id)
+VALUES ('Lenguaje de Programacion', 'Type Script', 'Basico', 1);
+
+INSERT INTO Skill (category, type, level, person_id)
+VALUES ('Otros', 'Comunicativa', 'No Aplica', 1);
